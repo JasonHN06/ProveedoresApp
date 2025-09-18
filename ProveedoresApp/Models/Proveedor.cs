87f89eb26bdf8104292513e9310b10a42e821ae5
Jason Madrid <jasonhnn6@gmail.com>
2025-09-18 15:22:45 -0600

    create model class

---
+++ ProveedoresApp/Models/Proveedor.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace ProveedoresApp.Models
+{
+    public class Proveedor
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Nombre { get; set; } 
+        public string Direccion { get; set; } 
+        public string Telefono { get; set; } 
+        public string Email { get; set; }
+    }
+}
