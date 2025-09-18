07302782a2a2eaed37655873e6837582217bc886
Jason Madrid <jasonhnn6@gmail.com>
2025-09-18 14:42:34 -0600

    Add project files.

---
+++ ProveedoresApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProveedoresApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
